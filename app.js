84350bd7133eec2cd9aee026b1ca618b78062013
Ruchiket Borse <RuchiketBorse1@gmail.com>
2022-05-31 07:39:23 +0530

    completed: load, search, more, download

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const searchInput = document.querySelector('.search-input')
 const searchBtn = document.querySelector('.search-btn');
 const searchForm = document.querySelector('.search-form')
 const contentDiv = document.querySelector('.content');
+const moreBtn = document.querySelector('.more')
 let searchValue;
+let searchCount = 2;
+let randomCount = 2;
 
-async function loadPhotos() {
-    const fetchData = await fetch('https://api.pexels.com/v1/curated?page=1&per_page=15', {
+async function fetchData(url) {
+    const fetchData = await fetch(url, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -18,8 +21,24 @@ async function loadPhotos() {
         console.log(data.error);
     } else {
         data.photos.forEach(photo => {
-            createPhotoDiv(photo)
+            createPhotoDiv(photo);
         })
+    }
+    toggleMoreBtn()
+}
+
+async function loadPhotos() {
+    await fetchData('https://api.pexels.com/v1/curated?page=1&per_page=15');
+
+}
+
+async function searchPhoto() {
+    clear();
+    if (searchValue !== '' || searchValue === undefined) {
+        searchValue = searchInput.value;
+        // request the photo which matches the search input
+        await fetchData(`https://api.pexels.com/v1/search?query=${searchValue}+query&page=1&per_page=15?orientation=square`)
+        searchInput.value = '';
     }
 }
 
@@ -27,31 +46,35 @@ function createPhotoDiv(photo) {
     let photoDiv = document.createElement('div');
     photoDiv.classList.add('photo-div');
     photoDiv.innerHTML = `
-    <img src=${photo.src.medium} alt=${photo.alt}></img>
-    <p>${photo.photographer}</p>`;
-    contentDiv.appendChild(photoDiv)
+    <img src=${photo.src.large} alt=${photo.alt}></img>
+    <div class="photo-desc"><p>${photo.photographer}</p> <a target="_blank" href=${photo.src.original} >Download</a></div>`;
+    contentDiv.appendChild(photoDiv);
 }
 
-async function searchPhoto() {
-    let query = searchInput.value;
-    // request the photo which matches the search input
-    if (query !== '') {
-        let fetchData = await fetch(`https://api.pexels.com/v1/search?query=${query}+query&page=1&per_page=15?orientation=portrait`, {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                Authorization: key
-            }
-        });
-        let data = await fetchData.json();
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            data.photos.forEach(photo => {
-                createPhotoDiv(photo)
-            })
-        }
-        searchInput.value = '';
+
+// remove items from content when search for new
+function clear() {
+    contentDiv.innerHTML = '';
+}
+
+async function loadMore() {
+
+    if (searchValue === '' || searchValue === undefined) {
+        await fetchData(`https://api.pexels.com/v1/curated?page=${searchCount}&per_page=15`)
+            ++randomCount;
+    } else {
+        await fetchData(`https://api.pexels.com/v1/search?query=${searchValue}+query&page=${searchCount}&per_page=15`);
+        ++searchCount;
+    }
+}
+
+
+
+function toggleMoreBtn() {
+    if (contentDiv.innerHTML === '') {
+        moreBtn.style.display = 'none';
+    } else {
+        moreBtn.style.display = 'block';
     }
 }
 searchBtn.addEventListener('click', (e) => {
@@ -59,4 +82,7 @@ searchBtn.addEventListener('click', (e) => {
     searchPhoto();
 });
 
-// loadPhotos();
+moreBtn.addEventListener('click', loadMore)
+
+loadPhotos();
+toggleMoreBtn()
